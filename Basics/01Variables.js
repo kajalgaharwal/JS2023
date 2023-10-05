db5d1e0929a6ce0095f051d4579ff5fb972c3a44
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2023-10-05 10:05:37 +0000

    variables

---
+++ Basics/01Variables.js
@@ -0,0 +1,7 @@
+const accountId = 1;
+let accountEmail = 'kajal@gmail.com';
+var accountPwd = '123';
+accountCity = 'ajmer';
+
+// accountId = 234;
+console.table([accountId,accountEmail,accountPwd,accountCity])
