180a275c61c0e84669b2d841772294c6ecd8e233
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2023-10-05 10:52:37 +0000

    operations

---
+++ Basics/03ConversionOperation.js
@@ -0,0 +1,17 @@
+let num = 33;
+let score = '33abc';
+let str = String(num);
+let str2 = Number(score);
+console.log(typeof str);
+console.log(typeof str2);
+console.log(str2);
+
+
+
+let x = 3;
+let y = x++;
+
+console.log(x,y);
+let a = 3;
+let b= ++a;
+console.log(a,b);
