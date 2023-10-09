f15bff1811e6d1ac468a5680fb4f595f2470b73a
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2023-10-09 12:15:14 +0000

    commit

---
+++ Basics/numbers.js
@@ -0,0 +1,15 @@
+let balance = new Number (100);
+console.log(balance.toFixed(2));
+let anothernumber = 123.57;
+console.log(anothernumber.toPrecision(4));
+let hundreds = 1000000;
+console.log(hundreds.toLocaleString('EN-IN'));
+
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);
+let max = 20;
+let min = 10;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
---
+++ Basics/strings.js
@@ -0,0 +1,9 @@
+let name = 'kajal'
+console.log(`hello! my name is ${name}`);
+let stringName = new String('kajalgaharwal');
+
+console.log(stringName.__proto__)
+console.log(stringName.charAt(2));
+console.log(stringName.includes('p'))
+console.log(stringName.substring(1,3));
+console.log(stringName.substring(3));
