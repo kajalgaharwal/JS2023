3567cd7a3ee318d630efb1ad882f66492d681d44
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2023-10-09 12:35:42 +0000

    date and time

---
+++ Basics/datendtime.js
@@ -0,0 +1,12 @@
+let date = new Date();
+console.log(date);
+console.log(date.toLocaleString());
+console.log(date.toDateString());
+console.log(date.toString());
+console.log(date.toTimeString());
+
+// let createdDate = new Date(2023,0,14);
+// let createdDate = new Date(2023,2,12,5,8);
+let createdDate = new Date('2023,01,23');
+console.log(createdDate);
+console.log(createdDate.toLocaleString());
