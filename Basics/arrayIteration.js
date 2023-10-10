65bfc942751487a7ce690b3399e0396cbeea295d
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2023-10-10 11:30:00 +0000

    iteration

---
+++ Basics/arrayIteration.js
@@ -0,0 +1,78 @@
+const number = [1,2,3,4,5];
+for (const num of number) {
+    // console.log(num);
+    
+}
+const greeting = 'hello world!';
+for (const greet of greeting) {
+    // console.log(greet);
+}
+
+//MAP
+
+const map = new Map();
+map.set('IN','INDIA');
+map.set('USA','United State of America');
+map.set('FR','France');
+// for (const [key,value] of map) {
+//     console.log(key +':'+value);
+// }
+
+
+//Oject //for in loop
+
+
+const obj = {
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'by apple'
+}
+// for (const key in obj) {
+//         console.log(obj[key] ,key);
+// }
+
+//forEach
+
+
+// const coding = ['python','js','cpp','c','java','ruby'];
+// coding.forEach(function (item,index,arr){
+//     console.log(item,index,arr);
+// })
+
+// coding.forEach((item)=> {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+// const coding = [
+//     {
+//         langName:'javascript',
+//         langFile:'js'
+//     },
+//     {
+//         langName:'java',
+//         langFile:'java'
+//     },
+//     {
+//         langName:'ruby',
+//         langFile:'rb'
+//     },
+//     {
+//         langName:'C++',
+//         langFile:'cpp'
+//     }
+// ]
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// })
+
+
+
---
+++ Basics/iife.js
@@ -0,0 +1,6 @@
+(function chai(){
+    console.log(`DB connected`);
+})();
+((name)=>{
+    console.log(`DB connected twoo ${name}`);
+})('kajal')
