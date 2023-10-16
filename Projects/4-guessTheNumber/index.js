76d0b3ef5af8d940b790823da471b85aff9e7cbf
Kajal Gaharwal <55242053+kajalgaharwal@users.noreply.github.com>
2023-10-16 13:02:49 +0000

    p

--- Projects/4-guessTheNumber/index.js
+++ Projects/4-guessTheNumber/index.js
@@ -6,6 +6,7 @@ const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 const startOver = document.querySelector('.resultParas');
+const p = document.createElement('p');
 let prevGuess = [];
 let numGuess = 1;
 let playGame = true;
